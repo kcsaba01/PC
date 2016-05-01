332c7c85ab251f61d1fe27b949f2c56d06a4c571
Csaba <0811994@rgu.ac.uk>
2016-05-01 01:10:35 +0100

    0

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 
 <body>
 <div class="main">
-<h1 style="font-family:Cambria, 'Hoefler Text', 'Liberation Serif', Times, 'Times New Roman', serif; font-size:32px;">Welcome to Photo Commenter</h1>
+<h1 style="font-family:Cambria, 'Hoefler Text', 'Liberation Serif', Times, 'Times New Roman', serif; font-size:32px;">Welcome to Photo Commenter (original)</h1>
     <div class="formbox">
         <h3>Login Form</h3>
         <br><br>
